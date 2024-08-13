72b0b3747340eb17ac945ca3f2944b6e0dc372d9
alejandro <ale_huller@hotmail.com>
2024-08-13 18:05:26 -0300

    Criando Person com sua service e controller

---
+++ 05_FirstStepsInJavaWithSpringBoot/api-with-spring-boot-and-java/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,53 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+@Service
+public class PersonServices {
+	
+	private static final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public List<Person> findAll() {
+		
+		logger.info("Finding every person!");
+		List<Person> persons = new ArrayList<>();
+		for(int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons ;
+	}
+
+	public Person findById(String id) {
+		
+		logger.info("Finding one person!");
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Alejandro");
+		person.setLastName("Huller");
+		person.setAddress("Sao Paulo - Sao Paulo - Brazil");
+		person.setGender("Male");
+		
+		
+		return person;
+	}
+	
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brazil" + i);
+		person.setGender("Male");
+		return person;
+	}
+}
