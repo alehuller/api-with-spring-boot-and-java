29ab2fcd2c82c117b967419dec72fb877a94d45f
alejandro <ale_huller@hotmail.com>
2024-08-19 19:28:33 -0300

    Adicionando o Migrations com Flyway

---
+++ 05_FirstStepsInJavaWithSpringBoot/api-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES 
+(1, 'Ayrton', 'Senna', 'Sao Paulo', 'Male'),
+(4, 'Leonardo', 'da Vinci', 'Anchiano - Italy', 'Male'),
+(6, 'Indira', 'Gandhi', 'Porbandar - India', 'Female'),
+(7, 'Mahatma', 'Gandhi', 'Porbandar - India', 'Male'),
+(9, 'Muhammad', 'Ali', 'Kentucky - USA', 'Male'),
+(11, 'Nelson', 'Mandela', 'Mvezo - South Africa', 'Male'),
+(12, 'Nicola', 'Tesla', 'Smiljan - Croatia', 'Male');
