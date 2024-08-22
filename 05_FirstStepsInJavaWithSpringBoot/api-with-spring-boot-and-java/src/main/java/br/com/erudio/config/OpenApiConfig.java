08687e7131c4a2d7c2186f5e3407a22e6940304b
alejandro <ale_huller@hotmail.com>
2024-08-22 14:14:03 -0300

    Adicionando o Swagger e suas configuracoes.

---
+++ 05_FirstStepsInJavaWithSpringBoot/api-with-spring-boot-and-java/src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package br.com.erudio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("RESTful API with Java and Spring Boot 3")
+						.version("v1")
+						.description("Description")
+						.termsOfService("Link with the terms of service.")
+						.license(
+								new License()
+								.name("Apache 2.0")
+								.url("URL")));
+	}
+	//http://localhost:8080/swagger-ui/index.html
+	//http://localhost:8080/v3/api-docs
+}
